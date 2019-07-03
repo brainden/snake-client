97fb4f153879233bf518e9081903a802721919f4
Brandon <brandonkilic@gmail.com>
2019-07-03 21:18:50 +0000

    working snake

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
     const conn = net.createConnection({ 
-      host: 'localhost',
-      port: 50541
+      host: IP,
+      port: PORT
     });
+    
     conn.on('connect',() => {
       conn.write('Name: abc');
      
